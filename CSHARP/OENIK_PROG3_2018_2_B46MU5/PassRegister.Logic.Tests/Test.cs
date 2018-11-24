00c05e25bc5a0b6895058a3fd3516cd43d57c4a1
Csuhaj Gergő <csuhajgergo@gmail.com>
2018-11-24 20:46:49 +0100

    PassRegister.Logic changes 2

---
+++ CSHARP/OENIK_PROG3_2018_2_B46MU5/PassRegister.Logic.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Test.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace PassRegister.Logic.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Test
+    {
+    }
+}
