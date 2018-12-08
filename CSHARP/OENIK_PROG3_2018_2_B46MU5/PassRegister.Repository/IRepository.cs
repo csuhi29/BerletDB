312d02c2bb656633903c9e2572aa2b1e55002f1a
Csuhaj Gergő <csuhajgergo@gmail.com>
2018-12-08 23:26:21 +0100

    Repository system reworked

--- CSHARP/OENIK_PROG3_2018_2_B46MU5/PassRegister.Repository/IRepository.cs
+++ CSHARP/OENIK_PROG3_2018_2_B46MU5/PassRegister.Repository/IRepository.cs
@@ -10,12 +10,36 @@ namespace PassRegister.Repository
     using System.Text;
     using System.Threading.Tasks;
 
-    public interface IRepository
+    /// <summary>
+    /// This is the Main Interface that is given to other Interfaces.
+    /// </summary>
+    /// <typeparam name="TEntity">This is a reference of the class that will be used.</typeparam>
+    public interface IRepository<TEntity>
+        where TEntity : class
     {
-        void Delete(int id);
+        /// <summary>
+        /// This method will Delete the entity.
+        /// </summary>
+        /// <param name="entity">Entity of the given type.</param>
+        void Delete(TEntity entity);
 
-        void Read();
+        /// <summary>
+        /// It will modify an item form a table.
+        /// </summary>
+        /// <param name="id"> The ID of the element we want to Modify.</param>
+        /// <param name="property">The name of the parameter of the ID given element.</param>
+        void Modify(int id, TEntity property);
 
-        void Modify(int id, string property);
+        /// <summary>
+        /// Returns the elements to a List where TEntity = the Table type.
+        /// </summary>
+        /// <returns>Returns table elements as a List.</returns>
+        List<TEntity> Read();
+
+        /// <summary>
+        /// Adda new Entity to the database.
+        /// </summary>
+        /// <param name="entity">Entity of the given type</param>
+        void Add(TEntity entity);
     }
 }
---
+++ CSHARP/OENIK_PROG3_2018_2_B46MU5/PassRegister.Repository/Repository.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="Repository.cs" company="CsuhajCompany">
+// Copyright (c) CsuhajCompany. All rights reserved.
+// </copyright>
+
+namespace PassRegister.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using PassRegister.Data;
+
+    /// <summary>
+    /// Repository class that implements IRepository interface.
+    /// </summary>
+    /// <typeparam name="TEntity">A database class.</typeparam>
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly PassRegisterDatabaseEntities dbe;
+
+        private readonly DbSet<TEntity> entities;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Repository{TEntity}"/> class.
+        /// The constructor of the Repository class.
+        /// </summary>
+        public Repository()
+        {
+            this.dbe = new PassRegisterDatabaseEntities();
+            this.entities = (DbSet<TEntity>)typeof(PassRegisterDatabaseEntities).GetProperty(typeof(TEntity).Name).GetValue(this.dbe);
+        }
+
+        /// <summary>
+        /// Adds a new element to the table.
+        /// </summary>
+        /// <param name="entity">The generic element of a table.</param>
+        public void Add(TEntity entity)
+        {
+            this.entities.Add(entity);
+            this.dbe.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deletes the element from the table.
+        /// </summary>
+        /// <param name="entity">The generic element of a table.</param>
+        public void Delete(TEntity entity)
+        {
+            this.entities.Remove(entity);
+            this.dbe.SaveChanges();
+        }
+
+        /// <summary>
+        /// Modify the given element from a given table.
+        /// </summary>
+        /// <param name="id">The key ID of the element.</param>
+        /// <param name="property">The element itself.</param>
+        public void Modify(int id, TEntity property)
+        {
+            TEntity temp = this.dbe.Set<TEntity>().Find(id);
+            this.dbe.Entry(temp).CurrentValues.SetValues(property);
+            this.dbe.SaveChanges();
+        }
+
+        /// <summary>
+        /// Reads all element from a given Table.
+        /// </summary>
+        /// <returns>The elements as a list.</returns>
+        public List<TEntity> Read()
+        {
+            return this.entities.ToList();
+        }
+    }
+}
